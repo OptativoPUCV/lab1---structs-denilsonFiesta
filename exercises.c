03ef4ae5ed57dca47981717de38fc29737521ff7
Replit user <>
2023-03-16 15:24:07 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -25,7 +25,7 @@ la cual encuentra el máximo y el mínimo valor del arreglo a y los
 almacena en las variables apuntadas por max y min.
 */
 void arrayMaxMin(int *a, int n, int *max, int *min) {
-    
+    *min = INT_MAX;
 }
 
 
